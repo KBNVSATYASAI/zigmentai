312c2831b33cf91c36c35711b808970b03da48f6
KBNVSATYASAI <2200030313@kluniversity.in>
2024-11-21 10:47:27 +0530

    db

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,18 @@ async function bootstrap() {
 }
 bootstrap();
 */
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Allow requests from all origins (modify as needed for security)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+  });
+
   await app.listen(3000);
 }
 
