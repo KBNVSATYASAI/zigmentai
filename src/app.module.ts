1b5ad8c087260411effd99c1c80f479ef1e2cd97
KBNVSATYASAI <2200030313@kluniversity.in>
2024-11-21 10:38:43 +0530

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotificationModule } from './modules/notification/notification.module'; // Import NotificationModule
 import { UserPreferencesModule } from './modules/user-preferences/user-preferences.module';
-
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/user_notifications'),
-    UserPreferencesModule,  // Ensure this is imported
-    NotificationModule,  // Ensure NotificationModule is imported
+    MongooseModule.forRoot(
+      'mongodb+srv://Balu:Balu123@cluster0.itvyruz.mongodb.net/<database_name>?retryWrites=true&w=majority&appName=Cluster0'
+    ),
+    UserPreferencesModule, // Ensure this is imported
+    NotificationModule, // Ensure NotificationModule is imported
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,13 @@ async function bootstrap() {
 }
 bootstrap();
 */
-import { NestFactory } from '@nestjs/core';
-import serverlessExpress from '@vendia/serverless-express';
-import { Handler } from 'aws-lambda';
-import { AppModule } from './app.module';
 
-let server: Handler;
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init(); // Initialize the application
-  const expressApp = app.getHttpAdapter().getInstance(); // Get Express instance
-  server = serverlessExpress({ app: expressApp }); // Use serverless-express
+  await app.listen(3000);
 }
 
-// Bootstrap the application
 bootstrap();
-
-// Export the handler for serverless execution
-export const handler: Handler = async (event, context) => {
-  if (!server) {
-    await bootstrap();
-  }
-  return server(event, context);
-};
